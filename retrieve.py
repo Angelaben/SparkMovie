41939be54ffbd5b08e09f8529815f31ef84ff41f
benjamin angelard <ikuritosensei@macbook-pro-de-benjamin.home>
2017-07-02 12:54:25 +0200

    Retrieve film ok

--- retrieve.py
+++ retrieve.py
@@ -16,20 +16,21 @@ import imdb
 
 
 i = imdb.IMDb()
-
-top250 = i.get_top250_movies()
-bottom100 = i.get_bottom100_movies()
-
 out_encoding = sys.stdout.encoding or sys.getdefaultencoding()
 
-for label, ml in [('top 10', top250[:10]), ('bottom 10', bottom100[:10])]:
-    print ''
-    print '%s movies' % label
-    print 'rating\tvotes\ttitle'
-    for movie in ml:
-        outl = u'%s\t%s\t%s' % (movie.get('rating'), movie.get('votes'),
-                                    movie['long imdb title'])
-        print outl.encode(out_encoding, 'replace')
+def testIMDB():
+    top250 = i.get_top250_movies()
+    bottom100 = i.get_bottom100_movies()
+
+
+    for label, ml in [('top 10', top250[:10]), ('bottom 10', bottom100[:10])]:
+        print ''
+        print '%s movies' % label
+        print 'rating\tvotes\ttitle'
+        for movie in ml:
+            outl = u'%s\t%s\t%s' % (movie.get('rating'), movie.get('votes'),
+                                        movie['long imdb title'])
+            print outl.encode(out_encoding, 'replace')
 
 # movie_list is a list of Movie objects, with only attributes like 'title'
 # and 'year' defined.
@@ -41,14 +42,18 @@ print first_match.summary()
 # update the information for this movie.
 i.update(first_match)
 # a lot of information will be printed!
-print first_match.summary()
+#print first_match.summary()
 # retrieve trivia information and print it.
-i.update(first_match, 'trivia')
-m = movie_list
-print m['trivia']
-# retrieve both 'quotes' and 'goofs' information (with a list or tuple)
-i.update(m, ['quotes', 'goofs'])
-print m['quotes']
-print m['goofs']
-# retrieve every available information.
-i.update(m, 'all')
+#i.update(first_match, 'trivia')
+print("-----DEBUT DE NOTRE PARTIE -------")
+def retrieveNmovie(n):
+
+    for movieID in range (30000, 30000+n):
+
+        movie = i.get_movie(str(movieID))
+        outl = u'%s\t%s\t%s' % (movie.get('rating'), movie.get('votes'),
+                                movie['long imdb title'])
+        print outl.encode(out_encoding, 'replace')
+        print movie.summary().encode(out_encoding, 'replace')
+# Calculer la note en divisant le rating par le nombre de vote ?
+retrieveNmovie(10)
