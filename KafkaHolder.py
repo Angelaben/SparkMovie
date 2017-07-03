bb19147b10474b36109c1b58a31cd04f23615d5e
benjamin angelard <ikuritosensei@MacBook-Pro-de-benjamin.local>
2017-07-03 14:52:18 +0200

    Part 1 in way

--- KafkaHolder.py
+++ KafkaHolder.py
@@ -4,18 +4,20 @@ import multiprocessing
 
 from kafka import KafkaConsumer, KafkaProducer
 
+debug = False
 
 class Producer(threading.Thread):
     daemon = True
 
     def run(self):
         producer = KafkaProducer(bootstrap_servers='localhost:9092')
-
-        while True:
-            producer.send('my-topic', b"test")
-            producer.send('my-topic', b"\xc2Hola, mundo!")
-            time.sleep(1)
-
+        if debug :
+            while True:
+                producer.send('my-topic', b"test")
+                producer.send('my-topic', b"\xc2Hola, mundo!")
+                time.sleep(1)
+        else:
+            producer.send('my-topic', b"Mundo does it all")
 
 class Consumer(multiprocessing.Process):
     daemon = True
@@ -26,7 +28,8 @@ class Consumer(multiprocessing.Process):
         consumer.subscribe(['my-topic'])
 
         for message in consumer:
-            print (message)
+
+            print ("Message in consumer ", message.value)
 
 
 def main():
@@ -42,7 +45,7 @@ def main():
 
 if __name__ == "__main__":
     logging.basicConfig(
-        format='%(asctime)s.%(msecs)s:%(name)s:%(thread)d:%(levelname)s:%(process)d:%(message)s',
+        format='%(name)s:%(thread)d:%(process)d:%(message)s',
         level=logging.INFO
         )
     main()
--- Mytmdb.py
+++ Mytmdb.py
@@ -1,6 +1,6 @@
 import tmdbsimple as tmdb
 tmdb.API_KEY = '913158e2a391c2720b155fdbe7cddce5'
-
+import json
 def retrieveData(n, path):
     fichier = open(path, "w")
     cpt = 0
@@ -11,9 +11,17 @@ def retrieveData(n, path):
             res = tmdb.Movies(i)
             movie = res.info()
             print(movie)
-            fichier.write(movie)
+            #fichier.write(movie)
+            #fichier.write("\n")
+
+
+            jsonarray = json.dumps(movie)
+
+            fichier.write(jsonarray)
             fichier.write("\n")
             cpt += 1
+            print("\n")
+            print("Lecture du ",cpt)
         except Exception:
             print("not found ")
     fichier.close()
