7afb9601f27c1cc81cba607d2c1f6029e0712517
benjamin angelard <ikuritosensei@MacBook-Pro-de-benjamin.local>
2017-07-04 16:12:19 +0200

    Analysis sentiment ok, propre

--- SentimentalAnalysis.py
+++ SentimentalAnalysis.py
@@ -6,16 +6,20 @@ from nltk.sentiment.util import *
 nltk.download('all')
 ############# OUR PART #############################
 from nltk import tokenize
-moviesList = []
-for line in open('parsed.txt', 'r'):
-    moviesList.append(json.loads(line))
-sid = SentimentIntensityAnalyzer()
 
-for movie in moviesList:
-    print(str(movie['review']))
-    if(movie['review']):
-        ss = sid.polarity_scores(str(movie['review']))
-        for k in sorted(ss):
-            print('{0}: {1}, '.format(k, ss[k]), end='')
-            print()
+def loader():
+    moviesList = []
+    for line in open('parsed.txt', 'r'):
+        moviesList.append(json.loads(line))
+    return moviesList
+def sentimentAnalysis(moviesList):
+    sid = SentimentIntensityAnalyzer()
+
+    for movie in moviesList:
+        print(str(movie['review']))
+        if(movie['review']):
+            ss = sid.polarity_scores(str(movie['review']))
+            for k in sorted(ss):
+                print('{0}: {1}, '.format(k, ss[k]), end='')
+                print()
 
