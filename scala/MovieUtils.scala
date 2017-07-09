8c036634f7d2092df388a21b7f946acd533222e6
Clara Ghabro <ghabro_c@epita.fr>
2017-07-09 15:01:11 +0200

    scala: write data from topic in local file

--- scala/MovieUtils.scala
+++ scala/MovieUtils.scala
@@ -6,7 +6,7 @@ import test.lol.GenreUtils._
 
 object MovieUtils {
   case class Movie(
-    adult:           Option[Boolean],
+    adult:              Option[Boolean],
  //   belongs_to_collection: Collection,
     budget:             Option[Int],
     genre:              Option[List[Genre]],
@@ -19,16 +19,17 @@ object MovieUtils {
     revenue:            Option[Int],
     runtime:            Option[Int],
     status:             Option[String],
-    tagline:            Option[String],
+    //tagline:            Option[String],
     title:              Option[String],
     vote_average:       Option[Float],
     vote_count:         Option[Int],
-    review:             Option[List[String]]
+    review:             Option[List[Option[String]]]
   )
 
   implicit val movieFormat = Json.format[Movie]
 
   def StringToMovie(str: String) = {
+    println(str)
     Json.parse(str).validate[Movie] match {
       case JsError(e)      => println(e); None
       case JsSuccess(t, _) => Some(t)
--- scala/main.scala
+++ scala/main.scala
@@ -1,26 +1,25 @@
 package test.lol
 
 import test.lol.MovieUtils._
+import test.lol.RatingUtils._
 //import test.lol.NewFile
-import org.apache.kafka.clients.consumer.ConsumerRecord
 import scala.collection.JavaConversions._
-import org.apache.kafka.clients.consumer.ConsumerRecords
 import org.apache.kafka.common.serialization.StringDeserializer
-import org.apache.kafka.clients.consumer.KafkaConsumer
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.rdd._
 import scala.collection.JavaConverters._
 import java.util.{Collections, Properties}
+import java.io._
 import kafka.message._
 import kafka.serializer._
 import kafka.utils._
-import org.apache.kafka.clients.consumer
 import java.util.Properties
 import kafka.utils.Logging
 import scala.collection.JavaConversions._
 import java.util
 
-import org.apache.kafka.clients.consumer.{ConsumerConfig, KafkaConsumer}
+import org.apache.kafka.clients.consumer.{ConsumerConfig, KafkaConsumer, ConsumerRecord}
+import org.apache.kafka.clients.consumer
 import org.apache.kafka.common.TopicPartition
 
 
@@ -40,11 +39,11 @@ object Main {
     props.put("zookeeper.connect", "localhost:2181")
     props.put("session.timeout.ms", "30000")
     props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
-              "org.apache.kafka.common.serialization.StringDeserializer")
+      "org.apache.kafka.common.serialization.StringDeserializer")
     props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
-              "org.apache.kafka.common.serialization.StringDeserializer")
+      "org.apache.kafka.common.serialization.StringDeserializer")
     props.put("partition.assignment.strategy",
-              "org.apache.kafka.clients.consumer.RangeAssignor")
+      "org.apache.kafka.clients.consumer.RangeAssignor")
     props
   }
 
@@ -54,13 +53,15 @@ object Main {
   def start(): Unit = {
     println("Start")
     val topics : util.List[String] = new util.ArrayList[String]()
-    topics.add("my-ratings")
+    //topics.add("my-rating")
+    topics.add("my-topic")
     consumer.subscribe(topics)
   }
 
   def toBeginning() = {
     val top = new util.ArrayList[TopicPartition]()
-    top.add(new TopicPartition("my-ratings", 2))
+    top.add(new TopicPartition("my-topic", 2))
+    //top.add(new TopicPartition("my-rating", 2))
     consumer.seekToBeginning(top)
   }
 
@@ -68,6 +69,7 @@ object Main {
     println("Show")
     val conf = new SparkConf().setAppName("testdelamortquitue").setMaster("local[*]")
     val sc = SparkContext.getOrCreate(conf)
+    /*
     val test = sc.textFile("reduced-tweets.json")
     println("====================================================================")
     println("====================================================================")
@@ -76,27 +78,50 @@ object Main {
 
     println("====================================================================")
     println("====================================================================")
-    while (true) {
-      println("Listening - scala ")
-      val records = consumer.poll(1000)
-      val it = records.records("my-ratings").iterator()
-      var fatLine = ""
-      while (it.hasNext) {
-        val record = it.next()
-        println("Message reçu : " + record.value())
-        fatLine.concat(record.value)
+    */
+   while (true) {
+     var fileWriter = new FileWriter("testFile.txt", true)
+     println("Listening - scala ")
 
-        val newLine = StringToMovie(record.value())
-      //  line.union(newLine)
-        println("====================================================================")
-        println("type d'objet: " + record.value().getClass)
-        println(newLine)
-        println("====================================================================")
-      }
-      Thread.sleep(1)
-  //    var rdd = fatLine.flatMap(StringToMovie)
-    }
+     val records = consumer.poll(1000)
+     var text = ""
+     val it = records.records("my-topic").iterator()
+     while (it.hasNext) {
+       val record = it.next()
+       fileWriter.write(record.value())
+       fileWriter.write("\n")
+       //text.concat(record.value())
+       //text.concat("\n")
+       println("test")
+     }
+
+    // if (text != "")
+    //   fileWriter.write(text)
+     fileWriter.flush()
+     fileWriter.close()
+     Thread.sleep(1000)
+
+     if (new File("testFile.txt").length() != 0) {
+       println ("parsing:")
+       var rdd = sc.textFile("testFile.txt")
+         .flatMap(StringToMovie)
+         //.flatMap(StringToRating)
+
+
+         if (rdd.take(1) != null) {
+           println("====================================================================")
+           println("====================================================================")
+           println(rdd.take(1))
+           println("====================================================================")
+           println("====================================================================")
+           // rdd.saveAsTextFile("anotherTest.txt")
+         }
+         println("GOOD")
+     }
+
+   }
   }
+
   def main(args: Array[String]): Unit = {
     println("babidoo")
     start()
