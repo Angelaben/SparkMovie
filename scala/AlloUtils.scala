6c265d8d5b5657dbc0a017a7184239dd1e07e8cb
benjamin angelard <ikuritosensei@MacBook-Pro-de-benjamin.local>
2017-07-10 15:46:47 +0200

    Allo ? alloo ? Chinzivonik

---
+++ scala/AlloUtils.scala
@@ -0,0 +1,21 @@
+package test.lol
+import play.api.libs.json._
+
+object AlloUtils  {
+
+  case class Allo (
+                     title:     String,
+                     ownRating : Option[Int],
+                     director : Option[String],
+                     spectators_reviews:             Option[List[Option[String]]],
+                     date : Option[String],
+                     id : Option[String]
+                   ) extends java.io.Serializable
+
+  implicit val movieFormat = Json.format[Allo]
+
+  def StringToAllo(str : String) = Json.parse(str).validate[Allo] match {
+    case JsError(e) => println(e); None
+    case JsSuccess(t, _) => Some(t)
+  }
+}
